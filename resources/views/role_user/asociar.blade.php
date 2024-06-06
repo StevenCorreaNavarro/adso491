95b0d15913a4a928be45e0b5980eb6b50ac8cdd5
LuisIrua23 <lairua@soy.sena.edu.co>
2024-06-06 00:07:44 -0500

    arreglado

--- resources/views/role_user/asociar.blade.php
+++ resources/views/role_user/asociar.blade.php
@@ -1,44 +1,53 @@
-vista asociar:
-
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="Description" content="Enter your description here" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+    <title>Registrar Profesor</title>
 </head>
 
 <body>
-
-    <h1>hola desde asociar:</h1>
-
-    <form action="{{ route('role_user.store') }}" method="POST" enctype="multipart/form-data">
-
-        @csrf
-
-
-        <br>
-        <h1>Usuarios:</h1>
-        <select name="user_id">
-            @foreach ($users as $user)
-                <option value="{{ $user->id }}">{{ $user->id }} - {{ $user->name }}</option>
-            @endforeach
-        </select>
-        <br>
-        <h1>Roles:</h1>
-        <select name="role_id">
-            @foreach ($roles as $rol)
-                <option value="{{ $rol->id }}">{{ $rol->id }} - {{ $rol->name }}</option>
-            @endforeach
-        </select>
-
-
-        <br> <br>
-        <button type="submit">Enviar Formulario:</button>
-    </form>
-
-
+    <div class="box-2">
+        <div class="container">
+            <center>
+                <br>
+                <h1>Hola desde asociar:</h1>
+                <br>
+                <br>
+                <form action="{{ route('role_user.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <br>
+                    <h3>Usuarios:</h3>
+                    <select name="user_id">
+                        @foreach ($users as $user)
+                            <option value="{{ $user->id }}">{{ $user->id }} - {{ $user->name }}</option>
+                        @endforeach
+                    </select>
+                    <br>
+                    <br>
+                    <h3>Roles:</h3>
+                    <select name="role_id">
+                        @foreach ($roles as $rol)
+                            <option value="{{ $rol->id }}">{{ $rol->id }} - {{ $rol->name }}</option>
+                        @endforeach
+                    </select>
+                    <br> <br>
+                    <button class="btn btn-primary" type="submit">Enviar Formulario:</button>
+                </form>
+                <br>
+                <br>
+                
+        </div>
+    </div>
+    </center>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
 </body>
 
 </html>
